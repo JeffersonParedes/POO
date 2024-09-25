dd73415d5016c2f45cf757d3a1fc15b43f174739
Alexrobles2002 <alexrobles084@gmail.com>
2024-09-25 02:32:08 -0500

    Update Pago.java

--- Documents/Lenguaje_Java_ciclo3_JeffersonParedesPretel/ProyectoFinal/src/proyectofinal/Pago.java
+++ Documents/Lenguaje_Java_ciclo3_JeffersonParedesPretel/ProyectoFinal/src/proyectofinal/Pago.java
@@ -9,5 +9,49 @@ package proyectofinal;
  * @author cliente
  */
 public class Pago {
-    
+private BigDecimal monto;
+private Date fechapago;
+private String metodoPago;
+
+public Pago(BigDecimal monto, Date fechapago, String metodoPago){
+    this.monto = monto;
+    this.fechapago = fechapago;
+    this.metodoPago = metodoPago;
+}
+
+public void procesarPago() {
+    System.out.println("Procesando el pago de " + monto + " mediante " + metodoPago + " en la fecha " + fechapago);
+}
+
+public void generarFactura() {
+    System.out.println("Generando factura por el pago de  " + monto + " realizado en la fecha " + fechapago);
+}
+
+public void reembolsarPago() {
+    System.out.println("Reembolsando el pago de " + monto + " realizado en la fecha " + fechapago);
+    }
+
+    public BigDecimal getMonto() {
+        return monto;
+    }
+
+    public void setMonto(BigDecimal monto) {
+        this.monto = monto;
+    }
+
+    public Date getFechapago() {
+        return fechapago;
+    }
+
+    public void setFechapago(Date fechapago) {
+        this.fechapago = fechapago;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
 }
